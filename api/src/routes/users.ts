adfc3b5761c0373f02e2e9ffdd888a53435b4e4a
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2022-11-18 15:39:22 -0500

    Update users.ts

--- api/src/routes/users.ts
+++ api/src/routes/users.ts
@@ -1,11 +1,12 @@
-import { AnyMxRecord } from "dns";
 import { Router, Request, Response, response } from "express";
-import { sanitizeFilter } from "mongoose";
-import { isPlusToken } from "typescript";
-import { addNewUser, compareUsernames, getAllUser, getUser, updateEmail, updateUser } from "../controllers/user/index";
-import { User } from '../models/User'
+import {
+  addNewUser,
+  getAllUser,
+  getUser,
+  updateEmail,
+  updateUser,
+} from "../controllers/user/index";
 import { mailOptionsRegister, transporter } from "../transport";
-const nodemailer = require("nodemailer");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const router = Router();
@@ -61,20 +62,23 @@ router.post("/login", async (req: Request, res: Response) => {
   }
 });
 
-
-router.get("/:username", userValidation, async (req: Request, res: Response) => {
-  try {
-    const { username } = req.params;
-    // comparar el username mandado con el que está en el token
-    // const authorization = req.get("authorization");
-    // const token = authorization?.split(" ")[1] as string;
-    // compareUsernames(username, token);
-    const user = await getUser(username);
-    res.status(200).send({ user })
-  } catch (error: any) {
-    res.status(500).send({ message: error.message })
+router.get(
+  "/:username",
+  userValidation,
+  async (req: Request, res: Response) => {
+    try {
+      const { username } = req.params;
+      // comparar el username mandado con el que está en el token
+      // const authorization = req.get("authorization");
+      // const token = authorization?.split(" ")[1] as string;
+      // compareUsernames(username, token);
+      const user = await getUser(username);
+      res.status(200).send({ user });
+    } catch (error: any) {
+      res.status(500).send({ message: error.message });
+    }
   }
-});
+);
 
 router.get("/admin", async (req: Request, res: Response) => {
   try {
@@ -117,18 +121,19 @@ router.get("/isUser", userValidation, async (req: Request, res: Response) => {
   }
 });
 
-
-router.put("/:username", userValidation, async (req: Request, res: Response) => {
-  try {
-    const { username, name, email, birthday } = req.body;
-    const updated = updateUser(username, { username, name, email, birthday });
-    if (!updated)
-      throw new Error('Usuario no encontrado');
-    res.status(200).send('Ok');
-  } catch (error: any) {
-    res.status(500).send({ message: error.message });
+router.put(
+  "/:username",
+  userValidation,
+  async (req: Request, res: Response) => {
+    try {
+      const { username, name, email, birthday } = req.body;
+      const updated = updateUser(username, { username, name, email, birthday });
+      if (!updated) throw new Error("Usuario no encontrado");
+      res.status(200).send("Ok");
+    } catch (error: any) {
+      res.status(500).send({ message: error.message });
+    }
   }
-})
+);
 
 export default router;
-
